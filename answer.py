749c0fb3c38f277c1dc2bee7d81d1e6dddc96814
Kazuke Ninja <162322212+kazukeninja@users.noreply.github.com>
2024-10-01 15:56:26 +0000

    Add files via upload
    add maj answer

---
+++ answer.py
@@ -0,0 +1,16 @@
+{
+  "youtube": {
+    "Watch YouTube Video #1": "070624",
+    "Major Games #1": "070624",
+    "Watch YouTube Shorts #3": "070624",
+    "Watch YouTube Shorts #4": "159390",
+    "Watch YouTube Shorts #6": "241086"
+  },
+  "answer": {
+    "choice_1": 13,
+    "choice_2": 7,
+    "choice_3": 6,
+    "choice_4": 5
+  },
+  "expires": 1727730000
+}
